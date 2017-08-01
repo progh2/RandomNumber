2e5c77285ea0bd14d0f21b9844e451291f966738
Gihun Ham <progh2@gmail.com>
2017-08-02 05:52:25 +0900

    버튼 클릭 기능 구현하기

--- app/src/main/java/com/example/randomnumber/MainActivity.java
+++ app/src/main/java/com/example/randomnumber/MainActivity.java
@@ -2,12 +2,26 @@ package com.example.randomnumber;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    TextView mTVNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mTVNumber = (TextView) findViewById(R.id.number);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Random random = new Random();
+        int maxInt = 6;
+        mTVNumber.setText( String.valueOf( random.nextInt() + 1) );
     }
 }
